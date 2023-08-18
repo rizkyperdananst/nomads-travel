ae5597653fb078db0c1a8a75a9b4ec7775cfbda4
rizkyperdananst <riskiperdana345@gmail.com>
2023-08-18 15:19:23 +0700

    NT-12/Detail-Transaction

---
+++ database/migrations/2023_08_18_081312_create_transaction_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('username');
+            $table->string('nationality');
+            $table->boolean('is_visa');
+            $table->date('doe_passport');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};

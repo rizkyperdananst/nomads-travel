022798205a7cb1c0bc5039c7a311ac942fddae61
rizkyperdananst <riskiperdana345@gmail.com>
2023-08-23 08:52:49 +0700

    NT-20/Integrasi-Midtrans

---
+++ config/midtrans.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'server_key' => env('MIDTRANS_SERVER_KEY', null),
+    'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
+    'isSanitized' => env('MIDTRANS_IS_SANITIZED', true),
+    'is3ds' => env('MIDTRANS_IS_3DS', true),
+];
+
+// config('midtrans.serverKey)
+// dan jangan lupa ketik php artisan config:clear

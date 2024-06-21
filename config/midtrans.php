b67f5d1dbb516a340ea42b79820d21a0b6c161b8
rizkyperdananst <riskiperdana345@gmail.com>
2024-06-22 00:58:32 +0700

    add-commit

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', null),
+    'serverKey' => env('MIDTRANS_SERVER_KEY', null),
     'isProduction' => env('MIDTRANS_IS_PRODUCTION', false),
     'isSanitized' => env('MIDTRANS_IS_SANITIZED', true),
     'is3ds' => env('MIDTRANS_IS_3DS', true),
